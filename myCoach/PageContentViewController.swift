84027cfce03fa50a890f07bd7a2374b1814487e3
João Fernando <joaofernando.costa@corp.globo.com>
2017-02-24 20:47:56 -0300

    Include facebook & google icons

---
+++ myCoach/PageContentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PageContentViewController.swift
+//  myCoach
+//
+//  Created by Joao on 2/24/17.
+//  Copyright © 2017 Mycoach. All rights reserved.
+//
+
+import UIKit
+
+class PageContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
