4ef896482580e83462b51bb29f6f64a565bc642f
João Fernando <joaofernando.costa@corp.globo.com>
2017-02-26 20:30:18 -0300

    Remove unnecessary lines

--- myCoach/InitialViewController.swift
+++ myCoach/InitialViewController.swift
@@ -12,7 +12,6 @@ class InitialViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
     }
 
@@ -20,6 +19,4 @@ class InitialViewController: UIViewController {
         super.didReceiveMemoryWarning()
         
     }
-    
-    
 }
