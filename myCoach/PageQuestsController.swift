40d1b270294a7be9b790bcda998e83ef103b85ea
João Fernando <joaofernando.costa@corp.globo.com>
2017-02-25 22:30:28 -0300

    WIP: Setting question pages layout, swipe, etc

---
+++ myCoach/PageQuestsController.swift
@@ -0,0 +1,97 @@
+//
+//  PageVc.swift
+//  myCoach
+//
+//  Created by Joao on 2/25/17.
+//  Copyright © 2017 Mycoach. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PageQuestsController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    lazy var VCArr: [UIViewController] = {
+        return [self.VCInstance(name: "QuestGender"),
+                self.VCInstance(name: "QuestShape"),
+                self.VCInstance(name: "QuestMainObjective"),
+                self.VCInstance(name: "QuestBadHabits"),
+                self.VCInstance(name: "QuestTime")]
+    }()
+    
+    private func VCInstance(name: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: name)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        self.delegate = self
+        if let questGender = VCArr.first {
+            setViewControllers([questGender], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        for view in self.view.subviews {
+            if view is UIScrollView {
+                view.frame = UIScreen.main.bounds
+            } else if view is UIPageControl {
+                view.backgroundColor = UIColor.clear
+            }
+        }
+    }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = VCArr.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return VCArr.last
+        }
+        
+        guard VCArr.count > previousIndex else {
+            return nil
+        }
+        
+        return VCArr[previousIndex]
+    }
+    
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = VCArr.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < VCArr.count else {
+            return VCArr.first
+        }
+        
+        guard VCArr.count > nextIndex else {
+            return nil
+        }
+        
+        return VCArr[nextIndex]
+    }
+
+    
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return VCArr.count
+    }
+    
+    
+    public func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+            let firstViewControllerIndex = VCArr.index(of: firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+}
