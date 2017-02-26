bb68801e8e1bc87417218bc42735ec7037aa75bf
João Fernando <joaofernando.costa@corp.globo.com>
2017-02-26 20:24:38 -0300

    Add methods to CustomButton class

--- myCoach/CustomButton.swift
+++ myCoach/CustomButton.swift
@@ -17,7 +17,40 @@ import UIKit
         self.layer.borderWidth = 1.0
         self.layer.borderColor = borderColor.cgColor
         self.titleEdgeInsets = UIEdgeInsetsMake(10, 10, 10, 10)
+    }
+    
+    internal func setActiveBgColor() {
+        let backgroundColor = UIColor(red:0.07, green:0.96, blue:0.62, alpha:1.0)
         
+        self.layer.backgroundColor = backgroundColor.cgColor
+    }
+    
+    internal func setActiveTitleColor() {
+        let titleColor:UIColor = UIColor(red:0.08, green:0.19, blue:0.25, alpha:1.0)
+        
+        self.setTitleColor(titleColor, for: .normal)
+    }
+    
+    internal func setInactiveBgColor() {
+        let backgroundColor = UIColor(red:0.08, green:0.19, blue:0.25, alpha:1.0)
+        
+        self.layer.backgroundColor = backgroundColor.cgColor
+    }
+    
+    internal func setInactiveTitleColor() {
+        let titleColor:UIColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.0)
+        
+        self.setTitleColor(titleColor, for: .normal)
     }
 
+    
+    public func setActive() {
+        setActiveTitleColor()
+        setActiveBgColor()
+    }
+    
+    public func setInactive() {
+        setInactiveTitleColor()
+        setInactiveBgColor()
+    }
 }
